10da61c38ec6adad1dd8d18ef364e67d5008653f
Mohammad Kaif <mkaifcse18@gmail.com>
2023-10-19 19:38:29 +0530

    first commit

---
+++ new_jarvis.py
@@ -0,0 +1,135 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import os
+import smtplib
+import send_email
+
+# Initializing Engine and Voices variables
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+# Defining Speak function
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+# Greet GM/GF/GE
+def wishMe():
+    current_hour = int(datetime.datetime.now().hour)
+    if current_hour >= 0 and current_hour < 12:
+        speak("Good Morning!")
+    elif current_hour >= 12 and current_hour < 18:
+        speak("Good Afternoon!")
+    else:
+        speak("Good Evening!")
+    speak("I'm Jarvis, sir. How may I help you?")
+
+# Taking command from the user
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source: 
+        print("Listening...")
+        r.pause_threshold = 0.6
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-US')
+        print("You said: ", query)
+        return query
+    except Exception as e:
+        print(e)
+        print("Say that again, please...")
+        return "None"
+
+# send email funtion 
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('mkaifcse18@gmail.com', 'zpgu kxyg wugi zdxx')
+    server.sendmail('mkaifcse18@gmail.com', to, content)
+    server.close()
+
+# ----------- search on google -------------
+def google_search(search_query):
+    search_url = f"https://www.google.com/search?q={search_query}"
+    webbrowser.open(search_url)
+
+
+# Main function
+if __name__ == "__main__":
+    wishMe() # to greet the user
+    while True:
+        query = takeCommand().lower()
+        print("You said:", query)  # to print the recognized command
+        
+        # Perform actions based on the recognized command
+        if 'wikipedia' in query:  #if wikipedia found in the query then this block will be executed
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2) 
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif "what's the time" in query:
+            current_time = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"The current time is {current_time}")
+
+        elif "who are you" in query:
+            speak("My name is Jarvis, I'm a virtual-assistant for automating various daily tasks on computer, such as sending email, opening any application, chatting, etc. I have been created by Mohammad Kaif.")
+
+        elif "open google" in query:
+            webbrowser.open("quora.com")
+
+        elif "open youtube" in query:
+            webbrowser.open("youtube.com")
+
+        elif "github profile" in query:
+            webbrowser.open("github.com/m-kaif07")
+        
+        elif "open quora" in query:
+            webbrowser.open("quora.com")
+        
+        elif "play music" in query:
+            music_dir = 'D:\\music'
+            songs = os.listdir(music_dir)
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif "open vs code" in query:
+            code_path = "C:\\Users\\moham\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(code_path)
+
+        elif "open web dev folder" in query:
+            file_path = "C:\\Users\\moham\\OneDrive\\Desktop"
+            os.startfile(file_path)
+
+        elif "open notepad" in query:
+            notepad_path = "C:\\Windows\\notepad.exe"
+            os.startfile(notepad_path)
+
+        elif "send email" in query:
+            try:
+                speak("whom I should send the email to? ")
+                recipient_name = takeCommand().lower()
+                speak('what should I send? ')
+                content = takeCommand()
+                send_email.sendEmail(recipient_name, content)
+                speak("Email has been sent!") 
+            except Exception as e:
+                print(e)
+                speak("sorry, I'm not able to sent email")
+
+        elif "search on google" in query:
+            speak("what should I search for sir?")
+            search_query = takeCommand()
+            google_search(search_query)
+
+        elif "exit" or "quit" in query:
+            speak("Goodbye!")
+            exit()
---
+++ send_email.py
@@ -0,0 +1,51 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+email = "mkaifcse18@gmail.com"
+password = "zpgu kxyg wugi zdxx"
+
+# Dictionary of names and email addresses
+recipient_dict = {
+    "kaif": "mohammadkaif5849@gmail.com",
+    "abuzar": "abuzarsk399@gmail.com",
+    "lambda": "lambda.z@yahoo.com"
+}
+
+# SMTP server settings (for Gmail)
+smtp_server = "smtp.gmail.com"
+smtp_port = 587
+
+# Creating a connection to the SMTP server
+def connect_to_smtp():
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(email, password)
+        return server
+    except smtplib.SMTPAuthenticationError:
+        print("Login failed. Check your email and password.")
+        return None
+
+# Function to send an email to a recipient
+def sendEmail(recipient_name, content):
+    if recipient_name in recipient_dict:
+        recipient_email = recipient_dict[recipient_name]
+
+        server = connect_to_smtp()
+        if server is not None:
+            message = MIMEMultipart()
+            message['From'] = email
+            message['To'] = recipient_email
+            message['Subject'] = "Hello, " + recipient_name
+
+            # what content to send the recipient
+            message.attach(MIMEText(content, 'plain'))
+
+            text = message.as_string()
+            server.sendmail(email, recipient_email, text)
+            print(f"Email sent to {recipient_name} ({recipient_email})")
+            server.quit()
+    else:
+        print(f"Recipient with the name '{recipient_name}' not found in the dictionary.")
+    
